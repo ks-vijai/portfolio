0727d635a83c989beedb633b71de2cbfe23999a6
ks-vijai <vijaiks.19cse@kongu.edu>
2023-02-07 14:50:11 +0530

    feat: home-page: add home page and styles
    Signed-off-by: ks-vijai <vijaiks.19cse@kongu.edu>

--- js/main.js
+++ js/main.js
@@ -2,15 +2,24 @@
 
 const menuButton = document.getElementById("menu-button");
 const hamburgerButton = document.getElementById("hamburger-button");
+const navBar = document.getElementById("navigation-bar");
+const menuNavBar = document.getElementById("menu-navigation-bar");
+const navBarItems = document.querySelectorAll(".menu-navigation-item");
 
 let showMenu = false;
 
 function toggleMenu() {
   if (!showMenu) {
     hamburgerButton.classList.add("open");
+    navBar.classList.add("open");
+    menuNavBar.classList.add("open");
+    navBarItems.forEach((item) => item.classList.add("open"));
     showMenu = true;
   } else {
     hamburgerButton.classList.remove("open");
+    navBar.classList.remove("open");
+    menuNavBar.classList.remove("open");
+    navBarItems.forEach((item) => item.classList.remove("open"));
     showMenu = false;
   }
 }
